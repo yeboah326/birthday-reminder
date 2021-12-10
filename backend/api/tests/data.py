fa2fe74f292fbde13c3a5e46a0bfdc1f7ec5e39b
Gideon Yeboah Asante <asanteg36@gmail.com>
2021-12-10 12:35:52 +0000

    Add new data for testing purposes

--- backend/api/tests/data.py
+++ backend/api/tests/data.py
@@ -2,19 +2,34 @@ super_user = {
     "name": "Super User",
     "email": "superu@email.com",
     "type": "super",
-    "password": "testing1234"
+    "password": "testing1234",
 }
 
 test_user_1 = {
     "name": "John Doe",
     "email": "johndoe@email.com",
     "type": "normal",
-    "password": "testing1234"
+    "password": "testing1234",
 }
 
 test_user_2 = {
     "name": "Jane Doe",
     "email": "janedoe@email.com",
     "type": "normal",
-    "password": "testing1234"
+    "password": "testing1234",
 }
+
+# Contacts
+valid_contacts = [
+    {"name": "Isabel Vasilevich", "date_of_birth": "2001-07-09", "relation": "sister"},
+    {"name": "Marshall Yeatman", "date_of_birth": "2001-10-30", "relation": "father"},
+    {"name": "Rogers McNirlin", "date_of_birth": "2001-02-13", "relation": "brother"},
+    {"name": "Bobinette Tollady", "date_of_birth": "2001-01-13", "relation": "friend"},
+    {"name": "Myca Berling", "date_of_birth": "2021-12-30", "relation": "friend"},
+    {"name": "Andrej Rudall", "date_of_birth": "2021-12-10", "relation": "friend"},
+    {"name": "Dalia Shannahan", "date_of_birth": "2021-12-16", "relation": "friend"},
+    {"name": "Amil Garlett", "date_of_birth": "2021-12-22", "relation": "friend"},
+    {"name": "Guy Haig", "date_of_birth": "2021-12-28", "relation": "friend"},
+]
+
+invalid_contact = [{"name": "Roselia Golda", "date_of_birth": "2001-10-01"}]
